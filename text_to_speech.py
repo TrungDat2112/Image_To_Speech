71ea6466f69d67ea3d33a7e2b1fc8df1b359f740
Trung Dat <nguyentrungdat6996@gmail.com>
2024-09-25 20:07:37 +0700

    preprocessing data and seq2seq class

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,1 +1,67 @@
-print("Hello")
+import pandas as pd
+from torchtext.data.utils import get_tokenizer
+from typing import Iterable
+from torchtext.vocab import build_vocab_from_iterator
+import re,string
+
+data_dir = "englishvietnamese"
+en_sents = open(data_dir + 'en_sents', "r").read().splitlines()
+vi_sents = open(data_dir + 'vi_sents', "r").read().splitlines()
+raw_data = {
+        "en": [line for line in en_sents[:170000]], # Only take first 170000 lines
+        "vi": [line for line in vi_sents[:170000]],
+    }
+df = pd.DataFrame(raw_data, columns=["en", "vi"])
+
+def word_tokenize(text):
+    tokens = re.findall(r'\w+|[^\w\s]', text, re.UNICODE)
+    return tokens
+
+def preprocessing(df): 
+    df["en"] = df["en"].apply(lambda ele: ele.translate(str.maketrans('', '', string.punctuation))) # Remove punctuation
+    df["vi"] = df["vi"].apply(lambda ele: ele.translate(str.maketrans('', '', string.punctuation)))  
+    df["en"] = df["en"].apply(lambda ele: ele.lower()) # convert text to lowercase
+    df["vi"] = df["vi"].apply(lambda ele: ele.lower())
+    df["en"] = df["en"].apply(lambda ele: ele.strip()) 
+    df["vi"] = df["vi"].apply(lambda ele: ele.strip()) 
+    df["en"] = df["en"].apply(lambda ele: re.sub("\s+", " ", ele)) 
+    df["vi"] = df["vi"].apply(lambda ele: re.sub("\s+", " ", ele))
+        
+    return df
+
+SRC_LANGUAGE = 'en'
+TGT_LANGUAGE = 'vi'
+
+# Place-holders
+token_transform = {}
+vocab_transform = {}
+
+# Tokenize for vietnames by underthesea
+def vi_tokenizer(sentence):
+    tokens = word_tokenize(sentence)
+    return tokens
+
+token_transform[SRC_LANGUAGE] = get_tokenizer('basic_english')
+token_transform[TGT_LANGUAGE] = get_tokenizer(vi_tokenizer)
+
+# helper function to yield list of tokens
+def yield_tokens(data_iter: Iterable, language: str):    
+    for index,data_sample in data_iter:
+        yield token_transform[language](data_sample[language])
+
+UNK_IDX, PAD_IDX, BOS_IDX, EOS_IDX = 0, 1, 2, 3
+# Make sure the tokens are in order of their indices to properly insert them in vocab
+special_symbols = ['<unk>', '<pad>', '<bos>', '<eos>']
+
+for ln in [SRC_LANGUAGE, TGT_LANGUAGE]:
+    # Training data Iterator
+    train_iter = df.iterrows()
+    # Create torchtext's Vocab object
+    vocab_transform[ln] = build_vocab_from_iterator(yield_tokens(train_iter, ln),
+                                                    min_freq=1,
+                                                    specials=special_symbols,
+                                                    special_first=True)
+
+
+for ln in [SRC_LANGUAGE, TGT_LANGUAGE]:
+  vocab_transform[ln].set_default_index(UNK_IDX)
